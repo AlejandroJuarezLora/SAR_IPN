c1c8c72fceb84a64fb2b9af26311d0986b52a876
AlejandroJuarezLora <jjuarezl2020@cic.ipn.mx>
2023-05-25 13:39:55 -0600

    ultimos cambios

--- xschem/sar/control/syn_spice.sh
+++ xschem/sar/control/syn_spice.sh
@@ -1,6 +1,5 @@
-read_verilog -DFUNCTIONAL -DSIM -DUSE_POWER_PINS -DUNIT_DELAY -lib /home/alex/pdk/sky130A/libs.ref/sky130_fd_sc_hd/verilog/sky130_fd_sc_hd.v
+read_verilog -DFUNCTIONAL -DSIM -DUSE_POWER_PINS -DUNIT_DELAY -lib /home/alex/pdk/sky130A/libs.ref/sky130_fd_sc_hd/verilog/sky130_fd_sc_hd.v #https://web.open-source-silicon.dev/t/424657/we-have-some-verilog-macros-defined-in-openram-defines-v-thi, https://github.com/YosysHQ/yosys/issues/3410
 
-#read_liberty -lib /home/alex/pdk/sky130A/libs.ref/sky130_fd_sc_hd/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 read_verilog sarlogic.v
 proc
 opt

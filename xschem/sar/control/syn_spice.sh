520ad133d49c8efb2921f3a2bf984ed8d9461e6c
AlejandroJuarezLora <jjuarezl2020@cic.ipn.mx>
2023-05-25 11:21:42 -0600

    expansion de puertos exitosa, pero ahora hay que ver que es el adc_buff

--- xschem/sar/control/syn_spice.sh
+++ xschem/sar/control/syn_spice.sh
@@ -1,4 +1,5 @@
-read_verilog -DUSE_POWER_PINS[=1] -lib /home/alex/pdk/sky130A/libs.ref/sky130_fd_sc_hd/verilog/sky130_fd_sc_hd.v
+read_verilog -DFUNCTIONAL -DSIM -DUSE_POWER_PINS -DUNIT_DELAY -lib /home/alex/pdk/sky130A/libs.ref/sky130_fd_sc_hd/verilog/sky130_fd_sc_hd.v
+
 #read_liberty -lib /home/alex/pdk/sky130A/libs.ref/sky130_fd_sc_hd/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 read_verilog sarlogic.v
 proc
@@ -12,6 +13,6 @@ opt
 dfflibmap -liberty /home/alex/pdk/sky130A/libs.ref/sky130_fd_sc_hd/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 abc -liberty /home/alex/pdk/sky130A/libs.ref/sky130_fd_sc_hd/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 splitnets -ports
-#opt
-#clean
+opt
+clean
 write_spice sar_logic_sky.sp
